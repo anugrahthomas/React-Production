67b8ba75b9ae1f3119f7acc454883590a7173074
Anugrah <thomasanuugrah1@gmail.com>
2026-02-12 19:24:35 +0530

    learning mordern ui libraries

---
+++ 03library/daisy/src/components/Buttons.tsx
@@ -0,0 +1,70 @@
+
+
+const Buttons = () => {
+  return (
+    <div className="pt-2">
+      <h2 className="font-semibold text-2xl">BUTTONS</h2>
+      <div className="px-2">
+        <h4>Solid Buttons</h4>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+      </div>
+
+      <div className="px-2">
+        <h4>Soft Buttons</h4>
+        <button className="btn btn-soft">Default</button>
+        <button className="btn btn-soft btn-primary">Primary</button>
+        <button className="btn btn-soft btn-secondary">Secondary</button>
+        <button className="btn btn-soft btn-accent">Accent</button>
+        <button className="btn btn-soft btn-info">Info</button>
+        <button className="btn btn-soft btn-success">Success</button>
+        <button className="btn btn-soft btn-warning">Warning</button>
+        <button className="btn btn-soft btn-error">Error</button>
+      </div>
+
+      <div className="px-2">
+        <h4>Outline Buttons</h4>
+        <button className="btn btn-outline">Default</button>
+        <button className="btn btn-outline btn-primary">Primary</button>
+        <button className="btn btn-outline btn-secondary">Secondary</button>
+        <button className="btn btn-outline btn-accent">Accent</button>
+        <button className="btn btn-outline btn-info">Info</button>
+        <button className="btn btn-outline btn-success">Success</button>
+        <button className="btn btn-outline btn-warning">Warning</button>
+        <button className="btn btn-outline btn-error">Error</button>
+      </div>
+
+      <div className="px-2">
+        <h4>Dash Buttons</h4>
+        <button className="btn btn-dash">Default</button>
+        <button className="btn btn-dash btn-primary">Primary</button>
+        <button className="btn btn-dash btn-secondary">Secondary</button>
+        <button className="btn btn-dash btn-accent">Accent</button>
+        <button className="btn btn-dash btn-info">Info</button>
+        <button className="btn btn-dash btn-success">Success</button>
+        <button className="btn btn-dash btn-warning">Warning</button>
+        <button className="btn btn-dash btn-error">Error</button>
+      </div>
+
+      <div className="px-2">
+        <h4>Active Buttons</h4>
+        <button className="btn btn-active">Default</button>
+        <button className="btn btn-active btn-primary">Primary</button>
+        <button className="btn btn-active btn-secondary">Secondary</button>
+        <button className="btn btn-active btn-accent">Accent</button>
+        <button className="btn btn-active btn-info">Info</button>
+        <button className="btn btn-active btn-success">Success</button>
+        <button className="btn btn-active btn-warning">Warning</button>
+        <button className="btn btn-active btn-error">Error</button>
+      </div>
+    </div>
+  );
+};
+
+export default Buttons;
